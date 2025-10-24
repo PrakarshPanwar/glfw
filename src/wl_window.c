1da049740dc6e1bb4c23be7435052587d7e15f1d
PrakarshPanwar <prakarshpanwar@gmail.com>
2025-10-24 20:03:28 +0530

    Added Cursor Shape Protocol
    - This protocol helps to make cursor object to match current theme in Plasma desktop
    - Defined dummy zwp_tablet_tool object to avoid undefined symbol errors

--- src/wl_window.c
+++ src/wl_window.c
@@ -51,6 +51,10 @@
 #include "xdg-activation-v1-client-protocol.h"
 #include "idle-inhibit-unstable-v1-client-protocol.h"
 #include "fractional-scale-v1-client-protocol.h"
+#include "cursor-shape-v1-client-protocol.h"
+
+// Dummy Tablet Tool Interface struct
+struct wl_interface zwp_tablet_tool_v2_interface = {};
 
 #define GLFW_BORDER_SIZE    4
 #define GLFW_CAPTION_HEIGHT 24
@@ -1228,9 +1232,65 @@ static GLFWbool createNativeSurface(_GLFWwindow* window,
     return GLFW_TRUE;
 }
 
+static GLFWbool setCursorShape(int shape)
+{
+    int xdgShape = -1;
+
+    switch (shape)
+    {
+        case GLFW_ARROW_CURSOR:
+            xdgShape = WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_DEFAULT;
+            break;
+        case GLFW_IBEAM_CURSOR:
+            xdgShape = WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_TEXT;
+            break;
+        case GLFW_CROSSHAIR_CURSOR:
+            xdgShape = WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_CROSSHAIR;
+            break;
+        case GLFW_POINTING_HAND_CURSOR:
+            xdgShape = WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_POINTER;
+            break;
+        case GLFW_RESIZE_EW_CURSOR:
+            xdgShape = WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_EW_RESIZE;
+            break;
+        case GLFW_RESIZE_NS_CURSOR:
+            xdgShape = WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_NS_RESIZE;
+            break;
+        case GLFW_RESIZE_NWSE_CURSOR:
+            xdgShape = WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_NWSE_RESIZE;
+            break;
+        case GLFW_RESIZE_NESW_CURSOR:
+            xdgShape = WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_NESW_RESIZE;
+            break;
+        case GLFW_RESIZE_ALL_CURSOR:
+            xdgShape = WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_ALL_SCROLL;
+            break;
+        case GLFW_NOT_ALLOWED_CURSOR:
+            xdgShape = WP_CURSOR_SHAPE_DEVICE_V1_SHAPE_NOT_ALLOWED;
+            break;
+        default:
+            break;
+    }
+
+    if (xdgShape == -1)
+        return GLFW_FALSE;
+
+    wp_cursor_shape_device_v1_set_shape(_glfw.wl.cursorShapeDevice,
+        _glfw.wl.pointerEnterSerial,
+        xdgShape);
+
+    return GLFW_TRUE;
+}
+
 static void setCursorImage(_GLFWwindow* window,
                            _GLFWcursorWayland* cursorWayland)
 {
+    if (_glfw.wl.cursorShapeDevice && cursorWayland->shape != 0)
+    {
+        if (setCursorShape(cursorWayland->shape))
+            return;
+    }
+
     struct itimerspec timer = {0};
     struct wl_cursor* wlCursor = cursorWayland->cursor;
     struct wl_cursor_image* image;
@@ -1927,6 +1987,9 @@ static void seatHandleCapabilities(void* userData,
     {
         _glfw.wl.pointer = wl_seat_get_pointer(seat);
         wl_pointer_add_listener(_glfw.wl.pointer, &pointerListener, NULL);
+
+        if (_glfw.wl.cursorShapeManager)
+            _glfw.wl.cursorShapeDevice = wp_cursor_shape_manager_v1_get_pointer(_glfw.wl.cursorShapeManager, _glfw.wl.pointer);
     }
     else if (!(caps & WL_SEAT_CAPABILITY_POINTER) && _glfw.wl.pointer)
     {
@@ -2819,6 +2882,7 @@ GLFWbool _glfwCreateCursorWayland(_GLFWcursor* cursor,
 GLFWbool _glfwCreateStandardCursorWayland(_GLFWcursor* cursor, int shape)
 {
     const char* name = NULL;
+    cursor->wl.shape = shape;
 
     // Try the XDG names first
     switch (shape)
@@ -3127,7 +3191,8 @@ void _glfwSetCursorWayland(_GLFWwindow* window, _GLFWcursor* cursor)
                 NULL,
                 0, 0,
                 0, 0,
-                0
+                0,
+                GLFW_ARROW_CURSOR
             };
 
             setCursorImage(window, &cursorWayland);
